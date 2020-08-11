22e9f9880288a292985faad5846d05126094c407
Anisha <anisha.0612@live.com>
2020-08-11 10:31:14 -0400

    Updates

---
+++ precache-manifest.dbfb46523fca482cfdd598afac500c61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dbbeb55a7a9f93301dabe8d5fb43bd2",
+    "url": "/Cats-inc/index.html"
+  },
+  {
+    "revision": "531d8a26213aabfa1d0b",
+    "url": "/Cats-inc/static/css/main.3a6fb35f.chunk.css"
+  },
+  {
+    "revision": "6fef48b16d50ebb80ec9",
+    "url": "/Cats-inc/static/js/2.3930d960.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Cats-inc/static/js/2.3930d960.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "531d8a26213aabfa1d0b",
+    "url": "/Cats-inc/static/js/main.85658952.chunk.js"
+  },
+  {
+    "revision": "1563a0fad1eff77c8f77",
+    "url": "/Cats-inc/static/js/runtime-main.f4b9030f.js"
+  }
+]);
